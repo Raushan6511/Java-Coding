3f83491b7bcffe8141196ec1eb862f11fc4c71d1
Raushan Kumar <raushankumar6511@gmail.com>
2025-07-23 21:29:00 +0530

    perform arithmetic operations

---
+++ BasicPrograms/ArithmeticOperation/ArithmeticOperation.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        int firstNumber=100;
+        int secondNumber=20;
+        System.out.println("Addition = "+(firstNumber+secondNumber));
+        System.out.println("Subtraction = "+(firstNumber-secondNumber));
+        System.out.println("Multiplication = "+(firstNumber*secondNumber));
+        System.out.println("Division = "+(firstNumber/secondNumber));
+    }
+}
