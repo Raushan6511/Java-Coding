9f1186c3387f4687ed46943d0ecd25ecb51e30e2
Raushan Kumar <raushankumar6511@gmail.com>
2025-07-24 23:19:13 +0530

    modified the file name as well class name

--- BasicPrograms/VowelOrConsonant/VowelOrConsoNant.java
+++ BasicPrograms/VowelOrConsonant/VowelOrConsoNant.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class VowelOrConsoNant {
+public class VowelOrConsonant {
     public static void main(String[] args) throws IOException{
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Please enter a character:");
