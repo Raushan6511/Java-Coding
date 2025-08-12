fa0786372d24305ca32d4c40589b4a3a65626551
Raushan Kumar <raushankumar6511@gmail.com>
2025-08-13 00:02:21 +0530

    slightly improvements

--- IterativePrograms/CountPrimeDigitsInNumbers/CountPrimeDigitsInNumbers.java
+++ IterativePrograms/CountPrimeDigitsInNumbers/CountPrimeDigitsInNumbers.java
@@ -19,14 +19,19 @@ public class CountPrimeDigitsInNumbers {
         }
         System.out.println("Total number of primes digits are: "+count);
     }
-    static boolean isPrime(int n){
-        if(n<=1) return false;
-        if(n==2) return true;
-        if(n%2==0) return false;
+    // static boolean isPrime(int n){
+    //     if(n<=1) return false;
+    //     if(n==2) return true;
+    //     if(n%2==0) return false;
         
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
-        }
-        return true;
+    //     for (int i = 3; i * i <= n; i += 2) {
+    //         if (n % i == 0) return false;
+    //     }
+    //     return true;
+    // }
+    
+    // improvement
+    static boolean isPrime(int n){
+        return n == 2 || n == 3 || n == 5 || n == 7;
     }
 }
