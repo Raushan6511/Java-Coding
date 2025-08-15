01985c33f6a0c4317cc3f58da823e3d0edd560b8
Raushan Kumar <raushankumar6511@gmail.com>
2025-08-15 21:58:32 +0530

    converting decimal to binary

---
+++ MathBasedPrograms/DecimalToBinary/DecimalToBinary.java
@@ -0,0 +1,20 @@
+package MathBasedPrograms.DecimalToBinary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DecimalToBinary {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please enter a number: ");
+        int decimalNumber=Integer.parseInt(br.readLine());
+        StringBuilder binaryForm=new StringBuilder();
+        while (decimalNumber>0) {
+            int remainder=decimalNumber%2;
+            decimalNumber=decimalNumber/2;
+            binaryForm.append(remainder);
+        }
+        System.out.println("Binary no of "+decimalNumber+" is: "+binaryForm.reverse().toString());
+    }
+}
