44e45b2ac06fdc9ad5a7aa375798c58b51ae0688
Raushan Kumar <raushankumar6511@gmail.com>
2025-08-25 23:57:23 +0530

    adding stream api modification

--- ArrayPrograms/RemoveDuplicateElement/RemoveDuplicateElement.java
+++ ArrayPrograms/RemoveDuplicateElement/RemoveDuplicateElement.java
@@ -1,29 +1,40 @@
 package ArrayPrograms.RemoveDuplicateElement;
 
+import java.util.Arrays;
+
 public class RemoveDuplicateElement {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 3, 4, 4, 5};
-        int arrayLength = arr.length;
+        // int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        // int arrayLength = arr.length;
 
-        int[] temp = new int[arrayLength];
-        int j = 0;
+        // int[] temp = new int[arrayLength];
+        // int j = 0;
 
-        for (int i = 0; i < arrayLength; i++) {
-            boolean found = false;
-            for (int k = 0; k < j; k++) {
-                if (arr[i] == temp[k]) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                temp[j++] = arr[i];
-            }
-        }
+        // for (int i = 0; i < arrayLength; i++) {
+        //     boolean found = false;
+        //     for (int k = 0; k < j; k++) {
+        //         if (arr[i] == temp[k]) {
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!found) {
+        //         temp[j++] = arr[i];
+        //     }
+        // }
+
+        // System.out.print("Unique Array: ");
+        // for (int i = 0; i < j; i++) {
+        //     System.out.print(temp[i] + " ");
+        // }
+
+        int[] arr = {1, 2, 9, 3, 4, 4, 5,6,9};
+        int[] unique = Arrays.stream(arr).distinct().toArray();
 
         System.out.print("Unique Array: ");
-        for (int i = 0; i < j; i++) {
-            System.out.print(temp[i] + " ");
+        for (int i = 0; i < unique.length; i++) {
+            System.out.print(unique[i] + " ");
         }
+        
     }
 }
