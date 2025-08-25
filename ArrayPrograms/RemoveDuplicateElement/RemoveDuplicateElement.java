c7000821227753f9d2cb49eb429c8705dbfb278c
Raushan Kumar <raushankumar6511@gmail.com>
2025-08-25 23:45:28 +0530

    removing duplicate element from array

---
+++ ArrayPrograms/RemoveDuplicateElement/RemoveDuplicateElement.java
@@ -0,0 +1,29 @@
+package ArrayPrograms.RemoveDuplicateElement;
+
+public class RemoveDuplicateElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        int arrayLength = arr.length;
+
+        int[] temp = new int[arrayLength];
+        int j = 0;
+
+        for (int i = 0; i < arrayLength; i++) {
+            boolean found = false;
+            for (int k = 0; k < j; k++) {
+                if (arr[i] == temp[k]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                temp[j++] = arr[i];
+            }
+        }
+
+        System.out.print("Unique Array: ");
+        for (int i = 0; i < j; i++) {
+            System.out.print(temp[i] + " ");
+        }
+    }
+}
